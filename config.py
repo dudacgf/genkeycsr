dd7dd4c651428dcd6d094be204cebfc2ceb5ac6b
Apache <apache@gamma-tauri.theshiresco.com>
2021-12-09 22:43:52 -0300

    Fix path to logo and favicon when running via mod_wsgi

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ GENKEYCSR_LOCALITY='Rio de Janeiro'
 GENKEYCSR_ORGNAME='Icatu Holding SA'
 GENKEYCSR_COMMON_NAME='CHANGE-ME.icatu.rede'
 GENKEYCSR_EMAIL='telecom@icatu.com.br'
-GENKEYCSR_LOGO='/static/images/logo_icatu.png'
-GENKEYCSR_FAVICON='/static/images/icatu-fav-icon.png'
+# logo and favicon are relative to static folder
+GENKEYCSR_LOGO='images/logo_icatu.png'
+GENKEYCSR_FAVICON='images/icatu-fav-icon.png'
 
