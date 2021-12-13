94387a0dbae9e6ac9e2326f4eb2b8bf328ded88b
Eduardo Ferreira <@dudacgf>
2021-12-12 22:23:26 -0300

    Adjust paths and settings according to README.md

--- docs/sample_wsgi.py
+++ docs/sample_wsgi.py
@@ -9,10 +9,10 @@ import site
 # python 3.9 (Ubuntu 20.04+, RedHat/CentOS/Rocky 8)
 site.addsitedir('/srv/genkeycsr/.env/lib/python3.9/site-packages')
 
+#
 # if you want to change default config values, copy the sample_config.py file under docs/ 
 # to the root of the environment and modify the options there
 #
-# you can also change the location of this file and set it here
 #
 os.environ['GENKEYCSR_CONFIG_PATH'] = '/srv/genkeycsr/config.py'
 
